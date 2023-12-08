1c5631d27012964055bbe9e9d3188b71d9bc9555
chno <ctnowak65@gmail.com>
2023-12-08 10:36:54 -0500

    [ADD] Added styling to the learning log platform as well as other small changes.

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -1,4 +1,4 @@
-'''Defines URL patterns for learning_logs.'''
+"""Defines URL patterns for learning_logs."""
 
 from django.urls import path
 
@@ -6,18 +6,17 @@ from . import views
 
 app_name = 'learning_logs'
 urlpatterns = [
-    #Home page
+    # Home page
     path('', views.index, name='index'),
     # Page that shows all topics.
     path('topics/', views.topics, name='topics'),
-    #Detail page for a single topic
+    # Detail page for a single topic.
     path('topics/<int:topic_id>/', views.topic, name='topic'),
-    #Page for adding a new topic
+    # Page for adding a new topic.
     path('new_topic/', views.new_topic, name='new_topic'),
-    #Page for adding a new entry.
+    # Page for adding a new entry.
     path('new_entry/<int:topic_id>/', views.new_entry, name='new_entry'),
-    #Page for editing an entry
+    # Page for editing an entry.
     path('edit_entry/<int:entry_id>/', views.edit_entry, name='edit_entry'),
 ]
-
 
